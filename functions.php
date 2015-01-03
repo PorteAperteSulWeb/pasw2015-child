eb31bc50e2b8f19d908cd0d2d1e5c4e6659b40fd
Christian <cghellere@libero.it>
2015-01-03 18:30:11 +0100

    update file
    aggiornato sistema di import template css

--- functions.php
+++ functions.php
@@ -2,6 +2,11 @@
 
 require (get_stylesheet_directory() . '/include/widget.php');
 
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' ); 
+function enqueue_parent_theme_style() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
 function pasw2015_child_widgets_init() {
 
     register_sidebar( array(
