d902786625ff3691cba046f065734ceea3170b2a
Christian Ghellere <cghellere@users.noreply.github.com>
2015-04-30 20:16:40 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
